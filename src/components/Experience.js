fff64d6f78daf336f514a6a0f32ad49d68590f8e
hkamran3 <hkamran3@uwo.ca>
2025-08-25 23:23:28 -0400

    More changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,21 +11,20 @@ function Experience() {
       tags: ['Python', 'SQL', 'Dash', 'Plotly', 'Docker', 'Cloud Run', 'Terraform'],
     },
     {
+      role: 'Product Strategy & Automation Consultant (Freelance)',
+      company: 'Onshore Advisors',
+      period: 'Apr 2025 — August 2025',
+      description: `Consolidated systems into a centralized dashboard, reducing onboarding time by 30%. Prototyped an AI-based client assistant using Figma to handle FAQs/onboarding and reduce manual calls. Automated key client lifecycle steps and documented workflows in Confluence, increasing overall process efficiency by 25% and improving user experience. Defined product requirements and managed tasks through Jira to ensure progress and visibility on workflow improvements designed for future scalability.`,
+      tags: ['Figma', 'Confluence', 'Jira', 'AI Automation', 'Dashboard Development', 'Workflow Optimization', 'Process Improvement']    
+    },
+    {
       role: 'Software Development Intern',
       company: 'J.D. Power',
       period: 'May 2023 — September 2024',
       description: `Increased system stability by 15% for 5,000+ daily users by fixing critical bugs and adding key features. Improved page load speed by 25%, enhancing UX for 10,000+ monthly users, by optimizing HTML/CSS, JavaScript, and React.js. Improved API efficiency by 25% and reduced server response times by 20% by optimizing SQL queries and developing back-end APIs in Java with Spring Boot. Reduced production bugs by 15% by implementing Playwright tests with 95% coverage.`,
       tags: ['JavaScript', 'Java', 'TypeScript', 'React', 'MSSQL', 'Springboot', 'PlayWright', 'Node.JS'],
     },
-    {
-      role: 'Software Engineer Intern',
-      company: 'Swimingo',
-      period: 'May 2022 — August 2022',
-      description: `Built and deployed a MEAN(MongoDB, Express, AngularJS, Node.js) stack lesson booking platform for a seed-stage startup, reducing manual bookings by 50% and increasing user satisfaction by 20%. Optimized MySQL queries, cutting lesson search and booking times by 30% for 2,000+ users. Implemented GCP auto-scaling and storage, reducing server downtime by 40% and lowering cloud costs by 20%.`,
-      tags: ['MongoDB', 'Express', 'AngularJS', 'Node.js', 'MySQL', 'GCP'],
-    },
 
-    // Add more experiences here...
   ];
 
   return (
